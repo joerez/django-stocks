ec0acbe1367d18f06d1c9a5866475175b183d9cc
JoeRez <rezenbiz@gmail.com>
2021-04-18 14:24:31 -0700

    parse everything

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -5,12 +5,12 @@ def home(request):
     import requests
     import json
     # pk_273fa449cb194ac1884930b5938ab43b
-    api_request = requests.get('https://cloud.iexapis.com/stable/stock/aapl/quote/latestPrice?token=pk_273fa449cb194ac1884930b5938ab43b')
+    api_request = requests.get('https://cloud.iexapis.com/stable/stock/aapl/quote/?token=pk_273fa449cb194ac1884930b5938ab43b')
     
     try:
         api = json.loads(api_request.content)
     except Exception as e:
-        api = "Error..." + e
+        api = "Error..."
 
 
     return render(request, 'home.html', {'api': api})
