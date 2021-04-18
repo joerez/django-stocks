64e72bb9b17b038a1b6992e3b80a2d6d2fabf15d
JoeRez <rezenbiz@gmail.com>
2021-04-18 14:45:40 -0700

    get stocks

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -4,16 +4,23 @@ from django.shortcuts import render
 def home(request):
     import requests
     import json
-    # pk_273fa449cb194ac1884930b5938ab43b
-    api_request = requests.get('https://cloud.iexapis.com/stable/stock/aapl/quote/?token=pk_273fa449cb194ac1884930b5938ab43b')
-    
-    try:
-        api = json.loads(api_request.content)
-    except Exception as e:
-        api = "Error..."
+
+    if request.method == 'POST':
+        ticker = request.POST['ticker']
+        api_request = requests.get('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote/?token=pk_273fa449cb194ac1884930b5938ab43b')
+        
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error..."
+
+        return render(request, 'home.html', {'api': api})
+
+    else:
+        return render(request, 'home.html', {'ticker': 'enter a ticker symbol above'})
 
 
-    return render(request, 'home.html', {'api': api})
+
 
 
 def about(request):
